fcb912b10f3ffae61ff44190135a7fcdd7de3755
William Bierhals <willambierhals>
2023-08-21 20:34:54 -0300

    :sparkles: Add first model and DAO setup

---
+++ models/User.php
@@ -0,0 +1,27 @@
+<?php 
+    // Classe para pegar todos os atributos do usuário
+    class User{
+        public $id;
+        public $name;
+        public $lastname;
+        public $email;
+        public $password;
+        public $image;
+        public $bio;
+        public $token;
+    }
+
+    interface UserDAOInterface{
+        public function buildUser($data);
+        public function create(User $user, $authUser= false);
+        public function update(User $user);
+        public function verifyToken($protected = false);
+        public function setTokenToSession($token, $redirect = true);    
+        public function authenticateUser($email, $password);
+        public function findByToken($token);
+        public function findByEmail($email);    
+        public function findById($id);
+        public function changePassword(User $user);    
+    }
+
+?>
