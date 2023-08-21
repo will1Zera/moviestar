d15c7fb49c575034c5cc4782d17d3679617f43e0
William Bierhals <willambierhals>
2023-08-21 17:25:46 -0300

    :recycle: Refactor to use the templates

--- index.php
+++ index.php
@@ -1,29 +1,11 @@
-<?php 
-    require_once("globals.php"); // Importa as variais para uso neste arquivo
-    require_once("db.php"); // Importa a conexão com o banco de dados
+<?php
+    require_once("templates/header.php");
 ?>
+    
+    <div id="main-container" class="container-fluid">
+        <h1>Body</h1>
+    </div>
 
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="img/moviestar.ico" />
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.1/css/bootstrap.css" integrity="sha512-azoUtNAvw/SpLTPr7Z7M+5BPWGOxXOqn3/pMnCxyDqOiQd4wLVEp0+AqV8HcoUaH02Lt+9/vyDxwxHojJOsYNA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- Icons font-awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- CSS do projeto -->
-    <link rel="stylesheet" href="css/styles.css" />
-    <title>MovieStar</title>
-</head>
-<body>
-    <h1>teste</h1>
-
-
-
-    <!-- Bootstrap js e JQuery -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.1/js/bootstrap.js" integrity="sha512-hidrIwTPV6f4zhtlHGC2IelVMrCqiHkHC9AuFJk22S95fMp5qnbhz2uLw5WH+lVWEWKg5nbRPkBsa4nNPGubsw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</body>
-</html>
+<?php
+    require_once("templates/footer.php");
+?>    
