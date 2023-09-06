9256c1e1f960585bdb1e232b2fe8a10bffe566e3
William Bierhals <willambierhals>
2023-09-06 11:05:27 -0300

    :recycle: Add other categories in the home

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     $latestMovies = $movieDao->getLatestMovies();
     $actionMovies = $movieDao->getMoviesByCategory("Ação");
     $comedyMovies = $movieDao->getMoviesByCategory("Comédia");
+    $romanceMovies = $movieDao->getMoviesByCategory("Romance");
+    $dramaMovies = $movieDao->getMoviesByCategory("Drama");
+    $fantasyMovies = $movieDao->getMoviesByCategory("Fantasia | Ficção");
 ?>
     
     <div id="main-container" class="container-fluid">
@@ -41,6 +44,39 @@
             <?php endforeach; ?>
             <?php if(count($comedyMovies) === 0): ?>
                 <p class="empty-list">Ainda não há filmes de comédia cadastrados.</p>
+            <?php endif; ?>
+        </div>
+
+        <h2 class="section-title">Romance</h2>
+        <p class="section-description">Veja os melhores filmes de romance</p>
+        <div class="movies-container">
+            <?php foreach($romanceMovies as $movie): ?>
+                <?php require("templates/movie_card.php"); ?>
+            <?php endforeach; ?>
+            <?php if(count($romanceMovies) === 0): ?>
+                <p class="empty-list">Ainda não há filmes de romance cadastrados.</p>
+            <?php endif; ?>
+        </div>
+
+        <h2 class="section-title">Drama</h2>
+        <p class="section-description">Veja os melhores filmes de drama</p>
+        <div class="movies-container">
+            <?php foreach($dramaMovies as $movie): ?>
+                <?php require("templates/movie_card.php"); ?>
+            <?php endforeach; ?>
+            <?php if(count($dramaMovies) === 0): ?>
+                <p class="empty-list">Ainda não há filmes de drama cadastrados.</p>
+            <?php endif; ?>
+        </div>
+
+        <h2 class="section-title">Fantasia | Ficção</h2>
+        <p class="section-description">Veja os melhores filmes de fantasia | ficção</p>
+        <div class="movies-container">
+            <?php foreach($fantasyMovies as $movie): ?>
+                <?php require("templates/movie_card.php"); ?>
+            <?php endforeach; ?>
+            <?php if(count($fantasyMovies) === 0): ?>
+                <p class="empty-list">Ainda não há filmes de fantasia | ficção cadastrados.</p>
             <?php endif; ?>
         </div>
     </div>
